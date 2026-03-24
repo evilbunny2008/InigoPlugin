acb14bded4a59ce271d411d2e73631e4592a5bc0
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2026-03-25 00:10:08 +1100

    refinements

--- bin/user/peak_detector.py
+++ bin/user/peak_detector.py
@@ -168,13 +168,14 @@ class PeakDetectorService(weewx.engine.StdService):
         stats = TimespanBinder(TimeSpan(int(midnight.timestamp()), int(now.timestamp())), self.db_lookup)
 
         OutTemp_max = stats.outTemp.max.raw
+        if OutTemp_max is not None:
 
-        if self.usUnit != weewx.US:
-            OutTemp_max = FtoC(OutTemp_max)
+            if self.usUnit != weewx.US:
+                OutTemp_max = FtoC(OutTemp_max)
 
-        record["OutTemp_max"] = round(OutTemp_max, 1)
+            record["OutTemp_max"] = round(OutTemp_max, 1)
 
-        log.info(f"{self.__class__.__name__} OutTemp_max: {round(stats.outTemp.max.raw, 1)}")
+            log.info(f"{self.__class__.__name__} OutTemp_max: {round(stats.outTemp.max.raw, 1)}")
 
     def reset_peak_detector(self):
 
