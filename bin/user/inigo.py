f194ca9ea9730a0ac49c4dc8763c73dc9fd4f7de
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2026-03-30 18:18:39 +1100

    refinements

--- bin/user/inigo.py
+++ bin/user/inigo.py
@@ -45,13 +45,15 @@ since_hour = 0
 last_report_ts = 0
 last_report = None
 
+REQUIRED_WEEWX = "5.3.0"
+
 if sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] < 7):
     raise weewx.UnsupportedFeature(
-        f"InigoService v{VERSION} requires Python 3.7 or later, found %s.%s" % (sys.version_info[0], sys.version_info[1]))
+        f"InigoService requires Python 3.7 or later, found %s.%s" % (sys.version_info[0], sys.version_info[1]))
 
-if weewx.__version__ < "4":
+if weeutil.weeutil.version_compare(weewx.__version__, REQUIRED_WEEWX) < 0:
     raise weewx.UnsupportedFeature(
-        f"InigoService v{VERSION} requires WeeWX 4 or later, found %s" % weewx.__version__)
+        f"InigoService v{VERSION} requires WeeWX 5.3.0 or later, found %s" % weewx.__version__)
 
 try:
     import numpy as np
