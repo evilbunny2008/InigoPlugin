c86b37a300156b28331e822456b44ddbe3f117cd
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2026-04-02 03:15:13 +1100

    Added more since rain periods

--- bin/user/inigo.py
+++ bin/user/inigo.py
@@ -723,8 +723,16 @@ class InigoSearchList(weewx.cheetahgenerator.SearchList):
 
         #group_rain = skin_dict.get("group_rain", "")
 
+        since_hour = -1
+        group_rain = "cm"
         skin_dict = self.generator.skin_dict
         if skin_dict is not None:
+
+            since_hour = int(float(skin_dict.get("since_hour", 0)))
+
+            if not 0 <= since_hour <= 23:
+                since_hour = 0
+
             units_dict = skin_dict.get("Units", None)
             if units_dict is not None:
                 groups_dict = units_dict.get("Groups", None)
@@ -732,11 +740,6 @@ class InigoSearchList(weewx.cheetahgenerator.SearchList):
                      group_rain = groups_dict.get("group_rain", None)
 
         log.info(f"group_rain: {group_rain}")
-
-        since_hour = int(float(skin_dict.get("since_hour", 0)))
-
-        if not 0 <= since_hour <= 23:
-            since_hour = 0
 
         log.info(f"since_hour: {since_hour}")
 
