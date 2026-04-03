89ca7c32ad99e1987da37ac0403f004aaf1487b4
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2026-04-03 16:50:23 +1100

    Fix final catch block output valie JSON

--- install.py
+++ install.py
@@ -11,7 +11,7 @@ import weewx
 from weecfg.extension import ExtensionInstaller
 from weeutil.config import conditional_merge
 
-VERSION="2.0.12"
+VERSION="2.0.13"
 
 InigoDataConfig = {
     "skin": "Inigo-Data",
