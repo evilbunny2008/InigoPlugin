03fd5f9b8955f3e67e95fb68a1917a6f32881748
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2026-04-04 20:20:20 +1100

    changes

--- bin/user/inigo.py
+++ bin/user/inigo.py
@@ -24,7 +24,7 @@ from pathlib import Path
 from weeutil.weeutil import TimeSpan, to_bool, to_float
 from weewx.reportengine import build_skin_dict, ReportTiming, set_cwd, set_locale
 from weewx.units import FtoC, getUnitGroup, ValueHelper
-from weewx.tags import AggTypeBinder, TimeBinder, TimespanBinder
+from weewx.tags import AggTypeBinder, ObservationBinder, TimeBinder, TimespanBinder
 
 log = logging.getLogger(__name__)
 
