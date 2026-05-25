9803fa79a8162e72f751915e4c00336f59b21766
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2026-05-25 17:32:09 +1000

    Version bump

--- install.py
+++ install.py
@@ -11,7 +11,7 @@ import weewx
 from weecfg.extension import ExtensionInstaller
 from weeutil.config import conditional_merge
 
-VERSION="2.0.19"
+VERSION="2.0.23"
 
 InigoDataConfig = {
     "skin": "Inigo-Data",
