cf5c5fbb15dd93f16485b60d7dd7c7c0d5761330
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2026-06-28 00:24:22 +1000

    Updated the dict and last templates

--- install.py
+++ install.py
@@ -11,7 +11,7 @@ import weewx
 from weecfg.extension import ExtensionInstaller
 from weeutil.config import conditional_merge
 
-VERSION="2.0.30"
+VERSION="2.0.31"
 
 InigoDataConfig = {
     "skin": "Inigo-Data",
